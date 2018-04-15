d439656c57b99f187ae6936f02ffa4618958cad9
Brenda Gonzalez <bremin11.20.93@gmail.com>
2018-04-15 17:29:35 -0500

    paqueterias

---
+++ src/components/home/HomeContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+
+    render() {
+        return (
+            <div>
+               ulala fifi frances :P
+            </div>
+        );
+    }
+}
+
+export default HomeContainer;
